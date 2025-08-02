ab67158fd2f787d5c3d77aef78415a9acb5020c0
Tauhid <tauhidahmed.dev@gmail.com>
2025-08-03 03:20:45 +0600

    Add navbar data structure for navigation links

---
+++ src/data/navbarData.ts
@@ -0,0 +1,9 @@
+import { homePath, howItWork, pricingPath } from "@/paths";
+
+const navbarData = [
+    { name: "Home", href: homePath },
+    { name: "How It Work", href: howItWork },
+    { name: "Pricing", href: pricingPath },
+]
+
+export default navbarData
