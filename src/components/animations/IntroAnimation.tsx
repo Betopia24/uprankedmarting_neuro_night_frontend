c3e8ddb93d412640f4f78b60e7bcbee13bef7fe9
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-08-07 03:08:08 +0600

    Implement code changes to enhance functionality and improve performance

---
+++ src/components/animations/IntroAnimation.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { cn } from "@/lib/utils";
+
+const step1 = {
+  hidden: { opacity: [1, 1, 0, 0, 0, 0] },
+};
+
+const step2 = {
+  hidden: (custom: "top" | "bottom" | "left" | "right" | "none") => {
+    const baseStyles = {
+      opacity: [0, 0, 0, 1, 1, 0, 0],
+      y:
+        custom === "top"
+          ? [20, 20, 20, 0, 0, 20, 20]
+          : custom === "bottom"
+          ? [20, 20, 20, 0, 0, 20, 20]
+          : [0, 0, 0, 0, 0, 0, 0],
+      x:
+        custom === "left"
+          ? [-20, -20, -20, 0, 0, -20, -20]
+          : custom === "right"
+          ? [20, 20, 20, 0, 0, 20, 20]
+          : [0, 0, 0, 0, 0, 0, 0],
+    };
+    return custom === "none" ? { ...baseStyles, y: 0, x: 0 } : baseStyles;
+  },
+};
+
+const variants = {
+  step1: step1,
+  step2: step2,
+};
+
+type IntroAnimationProps = {
+  variant: "step1" | "step2";
+  duration?: number;
+  followUp?: boolean;
+  custom?: "top" | "bottom" | "left" | "right" | "none";
+} & React.PropsWithChildren;
+
+export function IntroAnimation({
+  variant,
+  children,
+  duration = 6,
+  followUp = false,
+  custom = "none",
+}: IntroAnimationProps) {
+  return (
+    <motion.div
+      variants={variants[variant]}
+      animate="hidden"
+      transition={{
+        duration,
+        ease: "easeInOut",
+        repeat: Infinity,
+        repeatDelay: 1,
+      }}
+      className={cn(
+        followUp && "absolute inset-0 flex items-center justify-center"
+      )}
+      custom={custom}
+    >
+      {children}
+    </motion.div>
+  );
+}
--- src/components/marketing/Hero.tsx
+++ src/components/marketing/Hero.tsx
@@ -3,35 +3,93 @@ import Container from "../Container";
 import Heading from "../Heading";
 import Image from "next/image";
 import robotImage from "@/images/robot.png";
+import personImage from "@/images/person.png";
 import roboticEarphone from "@/images/robotic-earphone.png";
 import roboticPhone from "@/images/robotic-phone.png";
-import ZoomIn from "../animations/ZoomIn";
-
-// background: linear-gradient(180deg, #78A4FF 0%, #FFFFFF 34%, #EEF2FF 57%, rgba(1, 87, 255, 0.4) 100%);
-
-
+import { IntroAnimation } from "../animations/IntroAnimation";
 
 export default function Hero() {
-    return <div className="bg-gradient-to-b from-[#78A4FF] via-white via-[#EEF2FF] to-[#EEF2FF]">
-        <Container>
-            <div className="text-center py-12 relative flex flex-col gap-6 items-center">
-                <Heading size="6.5xl" weight="bold" className="leading-tight">Your Dynamic AI-Driven Virtual Receptionist & With Human.</Heading>
-                <div className="max-w-xl mx-auto space-y-6 lg:-mb-40">
-                    <p>Get the #1 rated receptionist service for small business.</p>
-                    <p>Never miss a call. Our AI and live receptionists answer 24/7, schedule appointments, and grow your business—all at a fraction of the cost.</p>
-                    <div className="space-y-2">
-                        <Button>Get Started For Free</Button>
-                        <div className="block text-xs">No credit card required*</div>
-                    </div>
-
-                </div>
-                <div className="flex justify-between w-full -mb-14 lg:-mb-60 pointer-events-none">
-                    <ZoomIn><Image className="size-28 lg:size-60 object-contain" src={roboticEarphone} alt="robot" /></ZoomIn>
-                    <ZoomIn delay={0.5}><Image className="size-28 lg:size-60 object-contain lg:mt-20" src={roboticPhone} alt="robot" /></ZoomIn>
-                </div>
-                <div className="mt-20"><ZoomIn delay={1}><Image className="object-contain max-h-84" src={robotImage} alt="robot" /></ZoomIn></div>
-
+  return (
+    <div className="bg-gradient-to-b from-[#78A4FF] via-white via-[#EEF2FF] to-[#EEF2FF] overflow-x-hidden">
+      <Container>
+        <div className="text-center py-12 relative flex flex-col gap-6 items-center">
+          <Heading size="6.5xl" weight="bold" className="leading-tight">
+            Your Dynamic AI-Driven Virtual Receptionist & With Human.
+          </Heading>
+          <div className="flex">
+            <div className="shrink-0 relative">
+              <IntroAnimation variant="step1">
+                <Image
+                  className="size-28 lg:size-60 object-contain"
+                  src={roboticEarphone}
+                  alt="robot"
+                />
+              </IntroAnimation>
+              <IntroAnimation
+                variant="step2"
+                duration={7}
+                custom="left"
+                followUp
+              >
+                <Image
+                  className="size-28 lg:size-60 object-contain"
+                  src={roboticEarphone}
+                  alt="robot"
+                />
+              </IntroAnimation>
             </div>
-        </Container>
+            <div className="flex-1 text-center space-y-6">
+              <p>Get the #1 rated receptionist service for small business.</p>
+              <p>
+                Never miss a call. Our AI and live receptionists answer 24/7,
+                schedule appointments, and grow your business—all at a fraction
+                of the cost.
+              </p>
+              <div className="space-y-2">
+                <Button>Get Started For Free</Button>
+                <div className="block text-xs">No credit card required*</div>
+              </div>
+            </div>
+            <div className="shrink-0 relative">
+              <IntroAnimation variant="step1">
+                <Image
+                  className="size-28 lg:size-60 object-contain lg:mt-20"
+                  src={roboticPhone}
+                  alt="robot"
+                />
+              </IntroAnimation>
+              <IntroAnimation
+                variant="step2"
+                duration={7}
+                custom="right"
+                followUp
+              >
+                <Image
+                  className="size-28 lg:size-60 object-contain lg:mt-20"
+                  src={roboticPhone}
+                  alt="robot"
+                />
+              </IntroAnimation>
+            </div>
+          </div>
+          <div className="lg:-mt-32 relative">
+            <IntroAnimation variant="step1">
+              <Image
+                className="object-contain max-h-84"
+                src={robotImage}
+                alt="robot"
+              />
+            </IntroAnimation>
+            <IntroAnimation variant="step2" duration={7} custom="top" followUp>
+              <Image
+                className="object-contain max-h-84"
+                src={personImage}
+                alt="client"
+              />
+            </IntroAnimation>
+          </div>
+        </div>
+      </Container>
     </div>
+  );
 }
