89a25ae83129cf8f62ca5b9371c128c0ca7c9584
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-08-23 04:40:05 +0600

    add chart dashboard

--- src/app/(dashboard)/dashboard/admin/page.tsx
+++ src/app/(dashboard)/dashboard/admin/page.tsx
@@ -1,5 +1,11 @@
 import CallGraph from "./_components/CallGraph";
+import CallGraphBarChart from "./_components/CallGraphBarChart";
 
 export default async function DashboardPage() {
-  return <CallGraph />;
+  return (
+    <>
+      <CallGraph />
+      <CallGraphBarChart />
+    </>
+  );
 }
