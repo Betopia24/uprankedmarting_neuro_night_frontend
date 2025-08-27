885b89d20d5827f29fa9d8081a11ab2598ee8a86
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-08-27 06:14:34 +0600

    fix type error

--- src/app/(dashboard)/dashboard/admin/page.tsx
+++ src/app/(dashboard)/dashboard/admin/page.tsx
@@ -1,10 +1,7 @@
 import CallGraph from "@/features/dashboard/CallGraph";
 import CallGraphBarChart from "@/features/dashboard/CallGraphBarChart";
-import fetchWithAuth from "@/lib/fetchWithAuth";
 
 export default async function DashboardPage() {
-  const d = await fetchWithAuth("/api/token", { method: "POST" });
-  console.log(d);
   return (
     <>
       <CallGraph />
