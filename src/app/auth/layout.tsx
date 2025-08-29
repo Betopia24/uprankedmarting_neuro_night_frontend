f0966c0953ac194c2a91682fb8ad56e3b0eae1e5
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-08-29 06:10:00 +0600

    make auth layout protected

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -1,3 +1,13 @@
-export default function AuthRootLayout({ children }: React.PropsWithChildren) {
+import { requireAuth } from "@/lib/auth";
+import { redirect } from "next/navigation";
+import { adminPaths } from "@/constants";
+
+export default async function AuthRootLayout({
+  children,
+}: React.PropsWithChildren) {
+  const auth: { data: { isVerified: boolean; role: string } } | null =
+    await requireAuth();
+  if (auth?.data?.isVerified)
+    return redirect(adminPaths[auth?.data?.role as keyof typeof adminPaths]);
   return children;
 }
