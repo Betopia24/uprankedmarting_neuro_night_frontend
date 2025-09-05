dff2d913eb3d88fb025a9d5b1db5a5c209f85800
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-09-05 06:42:24 +0600

    initial infinite scroll feature add

--- src/app/(dashboard)/dashboard/organization/agent-management/page.tsx
+++ src/app/(dashboard)/dashboard/organization/agent-management/page.tsx
@@ -5,6 +5,7 @@ import { getServerAuth } from "@/lib/auth";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
+import AgentsList from "./_components/AgentsList";
 
 // -----------------------------
 // Types
@@ -88,18 +89,7 @@ export default async function AgentManagementPath({ searchParams }: Props) {
   return (
     <div className="space-y-4">
       <Tabs selectedTab={viewParam} />
-
-      <div className="bg-gray-200 p-4 rounded-xl space-y-6">
-        <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-4 gap-6">
-          {users.map((user) => (
-            <AgentProfileCard
-              key={user.id}
-              user={user}
-              isSelected={viewParam === "my-agents"}
-            />
-          ))}
-        </div>
-      </div>
+      <AgentsList users={users} viewParam={viewParam} />
     </div>
   );
 }
@@ -130,84 +120,6 @@ function Tabs({ selectedTab }: { selectedTab: ViewType }) {
       >
         View My Agent List
       </Link>
-    </div>
-  );
-}
-
-// -----------------------------
-// Profile Card
-// -----------------------------
-
-export function AgentProfileCard({
-  user,
-  isSelected,
-}: {
-  user: AgentUser;
-  isSelected: boolean;
-}) {
-  return (
-    <div className="bg-white rounded shadow-xl p-4 overflow-hidden">
-      <div className="max-w-84 mx-auto space-y-3 text-center">
-        {user.image ? (
-          <Image
-            src={user.image}
-            alt={user.name}
-            className="w-24 h-24 rounded-full mx-auto"
-            width={96}
-            height={96}
-          />
-        ) : (
-          <div className="size-24 border border-gray-400 rounded-full mx-auto flex items-center justify-center text-4xl font-semibold">
-            {user.name.slice(0, 1)}
-          </div>
-        )}
-
-        <div>
-          <Heading size="h4" as="h4">
-            {user.name}
-          </Heading>
-          <span className="text-xs text-black/70 leading-none capitalize truncate">
-            {user.Agent.skills.slice(0, 2).join(", ")}
-          </span>
-        </div>
-
-        <div className="flex justify-center">
-          <RatingViewer rating={user.Agent.avgRating} />
-        </div>
-
-        <p className="text-xs">{user.bio}</p>
-
-        <div className="text-center">
-          {!isSelected ? (
-            <Button size="sm">Select</Button>
-          ) : (
-            <Button size="sm" className="bg-yellow-500 px-8">
-              Remove
-            </Button>
-          )}
-        </div>
-
-        <div className="flex items-center gap-2 justify-between flex-wrap border-t border-t-gray-200 py-4 px-8">
-          <Stats
-            progress={user.Agent.AgentFeedbacks.length}
-            label="Feedbacks"
-          />
-          <Stats progress={user.Agent.totalCalls} label="Total Calls" />
-        </div>
-      </div>
-    </div>
-  );
-}
-
-// -----------------------------
-// Stats
-// -----------------------------
-
-function Stats({ progress, label }: { progress: number; label: string }) {
-  return (
-    <div className="flex flex-col text-center">
-      <span className="text-base font-semibold">{progress}</span>
-      <span className="text-gray-500 text-xs">{label}</span>
     </div>
   );
 }
