b027e726e0764404b44ce94bb78def77d49c818a
Shoyas <md.nasiruddin4067@gmail.com>
2025-09-15 02:59:01 +0600

    feat(nasir-v2): need organization information to differentiate between subscripted or not

--- src/app/(dashboard)/dashboard/_components/DashboardSidebarContent.tsx
+++ src/app/(dashboard)/dashboard/_components/DashboardSidebarContent.tsx
@@ -1,3 +1,135 @@
+// "use client";
+
+// import { useSidebar } from "./SidebarProvider";
+// import { motion } from "framer-motion";
+// import DashboardActiveLink from "./DashboardActiveLink";
+// import { cn } from "@/lib/utils";
+
+// import {
+//   LayoutDashboard,
+//   Users,
+//   UserCog,
+//   PhoneCall,
+//   FileUp,
+//   Mic,
+//   Workflow,
+//   Settings,
+//   BarChart3,
+//   MessageSquare,
+//   Phone,
+//   CreditCard,
+//   Blocks,
+// } from "lucide-react";
+// import { useAuth } from "@/components/AuthProvider";
+// export type SidebarItemType = {
+//   name: string;
+//   href: string;
+//   icon: string;
+//   children?: SidebarItemType[];
+// };
+
+// type SidebarContentType = {
+//   mainItems: SidebarItemType[];
+//   subItems?: SidebarItemType[];
+// };
+
+// const icons = {
+//   layoutDashboard: LayoutDashboard,
+//   users: Users,
+//   userCog: UserCog,
+//   phoneCall: PhoneCall,
+//   fileUp: FileUp,
+//   mic: Mic,
+//   workflow: Workflow,
+//   settings: Settings,
+//   barChart3: BarChart3,
+//   messageSquare: MessageSquare,
+//   phone: Phone,
+//   creditCard: CreditCard,
+//   blocks: Blocks,
+// };
+
+// export default function SidebarContent({
+//   mainItems,
+//   subItems,
+// }: SidebarContentType) {
+//   const { isCollapsedSidebar } = useSidebar();
+
+//   const userInfo = useAuth();
+//   console.log("User Information: ", userInfo);
+
+//   return (
+//     <div className="h-full flex flex-col justify-between gap-6 px-2">
+//       <ul className={cn("space-y-2")}>
+//         {mainItems.map((item) => {
+//           const Icon = icons[item.icon as keyof typeof icons];
+//           return (
+//             <li key={item.name}>
+//               <DashboardActiveLink
+//                 className={cn("flex items-center whitespace-nowrap h-10")}
+//                 href={item.href}
+//               >
+//                 <span className="flex h-10 w-12 shrink-0 items-center justify-center">
+//                   <Icon className="size-4" />
+//                 </span>
+//                 <AnimatedLabel>{item.name}</AnimatedLabel>
+//               </DashboardActiveLink>
+//             </li>
+//           );
+//         })}
+//       </ul>
+//       {subItems && (
+//         <ul className={cn("space-y-2", isCollapsedSidebar && "space-y-4")}>
+//           {subItems.map((item) => {
+//             const Icon = icons[item.icon as keyof typeof icons];
+//             return (
+//               <li key={item.name}>
+//                 <DashboardActiveLink
+//                   className={cn("flex items-center whitespace-nowrap")}
+//                   href={item.href}
+//                 >
+//                   <span className="flex h-10 w-12 shrink-0 items-center justify-center">
+//                     <Icon className="size-4" />
+//                   </span>
+//                   <AnimatedLabel>{item.name}</AnimatedLabel>
+//                 </DashboardActiveLink>
+//               </li>
+//             );
+//           })}
+//         </ul>
+//       )}
+//     </div>
+//   );
+// }
+
+// const animatedLabelVariants = {
+//   collapsed: {
+//     opacity: 0,
+//   },
+//   expanded: {
+//     opacity: 1,
+//   },
+// };
+
+// function AnimatedLabel({ children }: React.PropsWithChildren) {
+//   const { isExpanded } = useSidebar();
+
+//   return (
+//     isExpanded && (
+//       <motion.span
+//         variants={animatedLabelVariants}
+//         initial="collapsed"
+//         animate="expanded"
+//       >
+//         {children}
+//       </motion.span>
+//     )
+//   );
+// }
+
+
+//! Try - 1
+
 "use client";
 
 import { useSidebar } from "./SidebarProvider";
@@ -20,11 +152,11 @@ import {
   CreditCard,
   Blocks,
 } from "lucide-react";
-// import { getSubscriptionType } from "@/app/api/subscription/subscription";
-// import { getServerAuth } from "@/lib/auth";
-// import { Button } from "@/components/ui/button";
-// import { Link } from 'next/link';
-// import { useAuth } from "@/components/AuthProvider";
+import { useAuth } from "@/components/AuthProvider";
+import { useEffect, useState } from "react";
+import { dashboardNavigation } from "@/data/dashboardNavbar";
+import { getSubscriptionType } from "@/app/api/subscription/subscription";
+import SkeletonSidebar from "@/components/SkeletonSidebar";
 
 export type SidebarItemType = {
   name: string;
@@ -54,42 +186,66 @@ const icons = {
   blocks: Blocks,
 };
 
-export default async function SidebarContent({
+export default function SidebarContent({
   mainItems,
   subItems,
 }: SidebarContentType) {
   const { isCollapsedSidebar } = useSidebar();
-  // const auth = await useAuth();
+  const auth = useAuth();
 
-  // if (!auth.token) {
-  //   console.error("Missing access token from getServerAuth()");
-  //   return <div className="text-red-500">Unauthorized: No access token</div>;
-  // }
+  const [orgMenu, setOrgMenu] = useState<SidebarItemType[] | null>(null);
+  const [loading, setLoading] = useState(false);
 
-  // let subscription;
-  // try {
-  //   subscription = await getSubscriptionType(auth.token);
-  // } catch (err) {
-  //   console.error("Error loading subscription:", err);
-  //   return (
-  //     <div className="flex items-center justify-center h-screen">
-  //       <div className="text-red-500 flex flex-col items-center">
-  //         <h1 className="text-2xl font-semibold">Buy a number first</h1>
-  //         {/* <Button variant={"link"} className="mt-4">
-  //           <a href="/dashboard/organization/explore-numbers">Explore Numbers</a>
-  //         </Button> */}
-  //         <Link href="/dashboard/organization/explore-numbers">Explore Numbers</Link>
-  //       </div>
-  //     </div>
-  //   );
-  // }
+  useEffect(() => {
+    const loadSubscription = async () => {
+      if (auth?.user?.role === "organization_admin" && auth?.token) {
+        setLoading(true);
+        try {
+          const subscription = await getSubscriptionType(auth.token);
 
-  // console.log(subscription);
+          if (!subscription || subscription.status !== "ACTIVE") {
+            setOrgMenu(dashboardNavigation.organization.slice(0, 2));
+          } else {
+            setOrgMenu(dashboardNavigation.organization);
+          }
+        } catch (error) {
+          setOrgMenu(dashboardNavigation.organization.slice(0, 2));
+        } finally {
+          setLoading(false);
+        }
+      }
+    };
+
+    loadSubscription();
+  }, [auth]);
+
+  let finalMainItems: SidebarItemType[] = [];
+  let finalSubItems: SidebarItemType[] = [];
+
+  if (auth?.user?.role === "super_admin") {
+    finalMainItems = dashboardNavigation.admin;
+  } else if (auth?.user?.role === "organization_admin") {
+    if (orgMenu) {
+      finalMainItems = orgMenu;
+      finalSubItems = dashboardNavigation.subItems.organization;
+    }
+  } else if (auth?.user?.role === "agent") {
+    finalMainItems = dashboardNavigation.agent;
+    finalSubItems = dashboardNavigation.subItems.agent;
+  } else {
+    finalMainItems = mainItems;
+    finalSubItems = subItems || [];
+  }
+
+  // subscription loading and orgMenu null then, skeleton
+  if (auth?.user?.role === "organization_admin" && (loading || !orgMenu)) {
+    return <SkeletonSidebar />;
+  }
 
   return (
     <div className="h-full flex flex-col justify-between gap-6 px-2">
       <ul className={cn("space-y-2")}>
-        {mainItems.map((item) => {
+        {finalMainItems.map((item) => {
           const Icon = icons[item.icon as keyof typeof icons];
           return (
             <li key={item.name}>
@@ -106,9 +262,10 @@ export default async function SidebarContent({
           );
         })}
       </ul>
-      {subItems && (
+
+      {finalSubItems.length > 0 && (
         <ul className={cn("space-y-2", isCollapsedSidebar && "space-y-4")}>
-          {subItems.map((item) => {
+          {finalSubItems.map((item) => {
             const Icon = icons[item.icon as keyof typeof icons];
             return (
               <li key={item.name}>
