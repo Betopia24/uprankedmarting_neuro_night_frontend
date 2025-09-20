2a4ab7ef4b19c240149aba53e99ea0253fd0d65f
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-09-21 02:31:22 +0600

    cleanup lenis

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,4 +1,3 @@
-import LenisProvider from "@/components/Lenis";
 import Footer from "@/components/landing/Footer";
 import Navbar from "@/components/landing/Navbar";
 
@@ -8,12 +7,10 @@ export default async function MarketingLayout({
   children: React.ReactNode;
 }) {
   return (
-    <LenisProvider>
-      <div className="min-h-screen flex flex-col">
-        <Navbar />
-        <main className="flex-1">{children}</main>
-        <Footer />
-      </div>
-    </LenisProvider>
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <main className="flex-1">{children}</main>
+      <Footer />
+    </div>
   );
 }
