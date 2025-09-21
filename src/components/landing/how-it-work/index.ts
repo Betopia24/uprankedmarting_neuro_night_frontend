7b16b9e8f045731b5a38f9cf513260f5d90c14af
Tauhid Ahmed <tauhidahmed.dev@gmail.com>
2025-09-22 02:41:02 +0600

    remove auto answer

--- src/components/landing/how-it-work/index.ts
+++ src/components/landing/how-it-work/index.ts
@@ -2,5 +2,4 @@ export { default as AiCallExperienceSection } from "./AiCallExperienceSection";
 export { default as AiAgentQuickStart } from "./AiAgentQuickStart";
 export { default as AiAgentCapabilities } from "./AiAgentCapabilities";
 export { default as AgentManagement } from "./AgentManagement";
-export { default as AutoAnswerNumberHighlight } from "./AutoAnswerNumberHighlight";
 export { default as FAQ } from "./Faq";
